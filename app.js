163a357599b0e0873680c58333c54755ee258049
BadalSherpa <sangay5chan@gmail.com>
2020-12-09 10:46:22 +0530

    updates codes

--- app.js
+++ app.js
@@ -184,6 +184,11 @@ app.post("/login", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+//connection Port
+let port = process.env.PORT;
+if (port == null || process == "") {
+  port = 3000;
+}
+app.listen(port, function () {
   console.log("Server started on port 3000.");
 });
